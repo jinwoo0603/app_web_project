d1d33c36a5c003d54cb51fa69e5466a67013f12c
jinwoo baek <jinwoo060360@gmail.com>
2024-11-28 13:34:39 +0900

    made basic structure for note router

--- routes/note.js
+++ routes/note.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
+router.get('/new', (req, res) => {
+});
+
 router.get('/:id', (req, res) => {
+});
+
+router.delete('/:id', (req, res) => {
+});
+
+router.get('/:id/edit', (req, res) => {
 });
 
 module.exports = router;
